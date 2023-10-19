bd20d39b455fb29544154c2c1524bf6802b62154
정승현 <tmdgusya@naver.com>
2023-10-19 18:05:51 +0900

    return health status with status code

--- backend/api/health.go
+++ backend/api/health.go
@@ -3,5 +3,6 @@ package api
 import "net/http"
 
 func HandleApiHealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
 }
